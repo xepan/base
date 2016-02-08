159d3deaf6b61919fc7f83fd16d861dd520cb890
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-08 21:18:43 +0530

    Added Addon Initiato

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -1,6 +1,13 @@
 <?php
 
-namespace xepanbase;
+namespace xepan\base;
 
 class Initiator extends \Controller_Addon {
+	public $addon_name = 'xepan_base';
+
+	function init(){
+		parent::init();
+		$this->routePages('xepan_base');
+		
+	}
 }
---
+++ page/contacts.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace xepan\base;
+
+
+class page_contacts extends \Page {
+
+	function init(){
+		parent::init();
+
+		$this->add('View')->set('Helloooooo');
+	}
+}
