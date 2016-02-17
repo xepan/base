b1e976f559b97cade2417d509295dba859b7714d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-17 23:02:02 +0530

    Added a global dummy Model_table in xEpan base app

---
+++ lib/Model/Table.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* description: ATK Model_Table is just left to put some global amendments needed in xEpan platform
+* 
+* @author : Gowrav Vishwakarma
+* @email : gowravvishwakarma@gmail.com, info@xavoc.com
+* @website : http://xepan.org
+* 
+*/
+
+namespace xepan\base;
+
+class Model_Table extends \Model_Table{}
