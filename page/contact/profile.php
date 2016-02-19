956799efd3ba97015ef3a4bf76e7a84bf51f793e
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-19 19:34:32 +0530

    Added View_Document psudo code of implementation

--- page/contact/profile.php
+++ page/contact/profile.php
@@ -17,11 +17,18 @@ class page_contact_profile extends \Page {
 	function init(){
 		parent::init();
 
-		// $cols = $this->add('Columns');
-		// $l = $cols->addColumn(3)->addStyle('width','30%');
-		// $cols->addColumn(6);
 		$contact = $this->add('xepan\base\Model_Contact')->load($this->api->stickyGET('id'));
-		$this->add('xepan\base\View_Profile',['action'=>$this->api->stickyGET('action')?:'view'])->setModel($contact,['first_name','last_name']);
+		$this->add('xepan\base\View_Profile',['action'=>$this->api->stickyGET('action')?:'view'])->setModel($contact,['first_name','last_name','type']);
+
+		$d = $this->add('xepan\base\View_Document',
+				[
+					'action'=>'view', // add/edit
+					'id_fields_in_view'=>'["all"]/["post_id","field2_id"]',
+					'allow_many_on_add' => false // Only visible if editinng
+				]
+			);
+		$d->setModel($contact,['view_field_here'],['edit_fields_here']);
+		$class_object  = $d->addMany('Emails_relation','on_spot','class_default_minicrud',['fields_grid','fields_form'],'class_options','template_for_class');
 		
 	}
 }
