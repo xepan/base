6d31abe07c830eea159801bcb2aeb094373548b0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-19 23:40:53 +0530

    View Document working now

--- lib/View/Document.php
+++ lib/View/Document.php
@@ -15,40 +15,98 @@ namespace xepan\base;
 
 class View_Document extends \View{
 
+	public $view_template = null;
 	public $action='view'; // add/edit
 	public $id_fields_in_view=[];
 	public $allow_many_on_add=true;
 
 	public $many_to_show=[];
+	public $view_fields=null;
+	public $form_fields=null;
 
+	public $form=null;
+
+	function defaultTemplate(){
+		if($this->action == 'view') 
+			return $this->view_template;
+		
+		return parent::defaultTemplate();
+	}
 
 
 	function init(){
 		parent::init();
-		
+		if(! $this->view_template) throw $this->exception("please provide template by 'view_template' option parameter");
+
+		if($this->action == 'view')
+			$this->form = new \Dummy();
+		else{
+			$this->form = $this->add('Form');
+			$this->form->setLayout($this->view_template);
+		}
 	}
 
 	function setModel($model,$view_fields=null,$form_fields=null){
-		return parent::setModel($model);
+		
+		$this->view_fields = $view_fields;
+		$this->form_fields = $form_fields;
+		
+		if($this->action=='view')
+			$fields = $view_fields;
+		else
+			$fields = $form_fields;
+
+		return parent::setModel($model,$fields);
 	}
 
 	function addMany(
 			$model,
 			$view_class='xepan\base\Grid',$view_options=null,$view_spot='Content',$view_defaultTemplate=null,$view_fields=null,
 			$class='xepan\base\MicroCRUD',$options=null,$spot='Content',$defaultTemplate=null,$fields=null
-		){
+		)
+	{
 
 		$view_prefix = '';
 		if($this->action=='view') $view_prefix='view_';
 
-		$v= $this->add(
+		$owner = $this;
+		if($this->action != 'view') $owner = $this->form->layout;
+
+		$v= $owner->add(
 				${$view_prefix.'class'},
 				${$view_prefix.'options'},
 				${$view_prefix.'spot'},
 				${$view_prefix.'defaultTemplate'}
 			);
+		$fields_2=null;
+		if($this->action == 'view'){
+			$fields_1 = $view_fields;
+		}else{
+			if(is_array($fields[0])){
+				$fields_1 = $fields[0];
+				$fields_2 = $fields[1];
+			}else{
+				$fields_1 = $fields[0];
+			}
+		}
+		$v->setModel($model,$fields_1,$fields_2);
+		$this->many_to_show[] = $v;
+		return $v;
+	}
 
-		$v->setModel($model,is_CRU)
+	function recursiveRender(){
+
+		if($this->action != 'view') {
+			$form = $this->form;
+			$form->setModel($this->model,$this->form_fields);
+
+			$form->onSubmit(function($f){				
+				$f->save();
+				return $this->js()->reload(['id'=>$f->model->id,'action'=>'view']);
+			});	
+		}
+
+		return parent::recursiveRender();
 	}
 
 
