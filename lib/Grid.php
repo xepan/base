3ee21042be47c9b6f7b6b01fc886105445aa6464
vijay mali <guddavijay@gmail.com>
2016-02-19 19:19:20 +0530

    base grid modified grid column delete formatter add

--- lib/Grid.php
+++ lib/Grid.php
@@ -15,8 +15,26 @@ class Grid extends \Grid{
 
 	public $template_option=null;
 
+	function init(){
+		parent::init();
+		$this->addHook('formatRow',function($grid){
+
+            if($grid->hasColumn('delete')){
+                $grid->columns['delete']['descr']='<a class="table-link danger" href="#"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-trash-o fa-stack-1x fa-inverse"></i></span></a>';
+            }
+            // if($grid->hasColumn('edit')){
+            // 	      $grid->columns['edit']['icon']='<i class="icon-'.
+            //         	$grid->columns['test ']['icon'].'"></i>';
+            //     // $grid->columns['edit']['descr']='<a class="table-link danger" href="#"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-pencil fa-stack-1x fa-inverse"></i></span></a>';
+            // }
+        });
+	}
 	function defaultTemplate(){
 		if($this->template_option) return $this->template_option;
 		return parent::defaultTemplate();
+	}
+	
+	function precacheTemplate(){
+
 	}
 }
