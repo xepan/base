531a115d55e3390b8b5d1d318c1ba7d9c1104071
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-23 10:14:11 +0530

    Controlelr ACL Added

---
+++ lib/Controller/ACL.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* description: xEpan ACL Controller, Realtion between document and contact
+* Still confused if it should be in HR or Here. Must not mix multiple applications anyhow.
+* 
+* @author : Gowrav Vishwakarma
+* @email : gowravvishwakarma@gmail.com, info@xavoc.com
+* @website : http://xepan.org
+* 
+*/
+
+class Controller_ACL extends \Abstract_Controller {
+
+	function init(){
+		parent::init();
+		
+	}
+}
