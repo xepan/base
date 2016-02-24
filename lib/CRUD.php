1590a8cb5502a75e5e5aba4c06a8e54c2dc180f8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-24 09:48:02 +0530

    Modified Grid and CRUD classes for xepan requirements

--- lib/CRUD.php
+++ lib/CRUD.php
@@ -5,6 +5,7 @@ class CRUD extends \CRUD{
 	
 	public $grid_class='xepan\base\Grid';
 	public $action_page=null;
+	public $pass_acl=false;
 
 	protected function configureAdd($fields){
 		if($this->action_page){
@@ -24,8 +25,21 @@ class CRUD extends \CRUD{
 					]
 				]
 			);
+		}else{
+			// $this->grid->columns['delete']['template']= '<a class="table-link danger do-delete" href="#" data-id="'.$this->model->id.'"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-trash-o fa-stack-1x fa-inverse"></i></span></a>';
+		 //    $this->grid->columns['edit']['template']= '<a class="table-link pb_edit" href="#" data-id="'.$this->model->id.'"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-pencil fa-stack-1x fa-inverse"></i></span></a>';
+			parent::configureEdit($fields);
 		}
-		// parent::configureEdit($fields);
+	}
+
+	function setModel($model,$grid_fields=null,$form_fields=null){
+
+		$m = parent::setModel($model,$grid_fields,$form_fields);
+		
+		if((($m instanceof \xepan\base\Model_Document) || ($m instanceof \xepan\base\Model_Contact)) && !$this->pass_acl){
+			$this->add('xepan\hr\Controller_ACL');
+		}
+		return $m;
 	}
 
 }
--- lib/Grid.php
+++ lib/Grid.php
@@ -14,6 +14,8 @@ namespace xepan\base;
 class Grid extends \Grid{
 
 	public $defaultTemplate=null;
+	public $row_edit=true;
+	public $row_delete=true;
 
 	function init(){
 		parent::init();
@@ -31,15 +33,18 @@ class Grid extends \Grid{
 	}
 
 	function formatRow(){
-		
-	 //    $this->columns['edit']['icon'] = '<i class="x fa fa-pencil fa-inverse" ></i>&nbsp;';
-	 //    $this->columns['edit']['descr'] = "";
-		// // throw new \Exception($this->columns['edit']['icon']);
-	 //    parent::formatRow();
 
-	    $this->current_row_html['delete']= '<a class="table-link danger do-delete" href="#" data-id="'.$this->model->id.'"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-trash-o fa-stack-1x fa-inverse"></i></span></a>';
-	    $this->current_row_html['edit']= '<a class="table-link pb_edit" href="#" data-id="'.$this->model->id.'"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-pencil fa-stack-1x fa-inverse"></i></span></a>';
+	    parent::formatRow();
 
+	    if($this->row_edit)
+		    $this->current_row_html['edit']= '<a class="table-link pb_edit" href="#" data-id="'.$this->model->id.'"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-pencil fa-stack-1x fa-inverse"></i></span></a>';
+		else
+			$this->current_row_html['edit']= '<span class="fa-stack table-link"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-pencil fa-stack-1x fa-inverse"></i></span>';
+
+		if($this->row_delete)
+		    $this->current_row_html['delete']= '<a class="table-link danger do-delete" href="#" data-id="'.$this->model->id.'"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-trash-o fa-stack-1x fa-inverse"></i></span></a>';
+		else
+			$this->current_row_html['delete']='';
 	}
 
 }
