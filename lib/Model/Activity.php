4603d2d04be13837945477df7c9755b7fdacfc0f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-26 00:13:28 +0530

    Added activity model

--- lib/Model/Activity.php
+++ lib/Model/Activity.php
@@ -19,10 +19,11 @@ class Model_Activity extends xepan\base\Model_Table{
 
 		$this->hasOne('xepan\base\Epan');
 		$this->hasOne('xepan\base\Contact');
-
-		$this->hasOne('xepan\base\Document');
+		$this->hasOne('xepan\base\Contact','related_contact_id');
+		$this->hasOne('xepan\base\Document','related_document_id');
 
 		$this->addField('activity');
+		$this->addField('details');
 
 		$this->addField('created_at')->type('datetime')->defaultValue($this->api->now);
 	}
