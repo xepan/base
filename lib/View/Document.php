82d536d92712e4c923868e156795257ff905cbf8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-26 15:46:29 +0530

    Bug fixed

--- lib/View/Document.php
+++ lib/View/Document.php
@@ -88,20 +88,20 @@ class View_Document extends \View{
 		$owner = $this;
 		if($this->action != 'view') $owner = $this->form->layout;
 
-		$tmpl = [];
-		if(!${$view_prefix.'defaultTemplate'}) {
-			$tmpl = ['defaultTemplate'=>${$view_prefix.'defaultTemplate'}];
-			if($class == 'xepan\base\CRUD')
-				$tmpl = ['grid_options'=>$tmpl];
-		}
+		// $tmpl = [];
+		// if(!${$view_prefix.'defaultTemplate'}) {
+		// 	$tmpl = ['defaultTemplate'=>${$view_prefix.'defaultTemplate'}];
+		// 	if($class == 'xepan\base\CRUD')
+		// 		$tmpl = ['grid_options'=>$tmpl];
+		// }
 
-		if(!${$view_prefix.'options'}){
-			${$view_prefix.'options'}=[];
-		}
+		// if(!${$view_prefix.'options'}){
+		// 	${$view_prefix.'options'}=[];
+		// }
 
 		$v= $owner->add(
 				${$view_prefix.'class'},
-				array_merge(${$view_prefix.'options'},$tmpl),
+				${$view_prefix.'options'},
 				${$view_prefix.'spot'},
 				${$view_prefix.'defaultTemplate'}
 			);
