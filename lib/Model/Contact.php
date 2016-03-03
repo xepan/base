ec9adf738c27995a4fe7e1b6a6ff962e362993d2
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-04 01:01:34 +0530

    Added avatar from Contact Model

--- lib/Model/Contact.php
+++ lib/Model/Contact.php
@@ -40,6 +40,8 @@ class Model_Contact extends \xepan\base\Model_Table{
 		$this->addField('post');
 		$this->addField('website');
 
+		$this->add('filestore/Field_Image','image_id')->allowHTML(true);
+
 		$this->addExpression('name')->set($this->dsql()->expr('CONCAT([0]," ",[1])',[$this->getElement('first_name'),$this->getElement('last_name')]));
 
 		$this->hasMany('xepan\base\Contact_Email',null,null,'Emails');
@@ -47,11 +49,6 @@ class Model_Contact extends \xepan\base\Model_Table{
 		$this->hasMany('xepan\base\Contact_Relation',null,null,'Relations');
 		$this->hasMany('xepan\base\Contact_IM',null,null,'IMs');
 		$this->hasMany('xepan\base\Contact_Event',null,null,'Events');
-
-		// $this->addExpression('email')->set(function($m,$q){
-		// 	$x = $m->add('xepan\base\Model_Contact_Info',['table_alias'=>'one_email']);
-		// 	return $x->addCondition('contact_id',$q->getField('id'))->setLimit(1)->fieldQuery('value');
-		// });
 
 		$this->addExpression('emails_str')->set(function($m,$q){
 			$x = $m->add('xepan\base\Model_Contact_Email',['table_alias'=>'emails_str']);
@@ -64,6 +61,17 @@ class Model_Contact extends \xepan\base\Model_Table{
 		})->allowHTML(true);
 
 
+		$this->addExpression('avatar','""')->allowHTML(true);
+		$this->addHook('afterLoad',function($m){
+			if(!$m['image_id']){
+				$m['avatar'] = "<div class='namebadge' style='position:relative; max-width:50px;float:left'>".$m['first_name'].' '.$m['last_name']."</div>";
+			}
+		});
+
+		if(!isset($this->app->avatar_loaded)){
+			$this->app->js(true)->_load('jquery.nameBadges')->_selector('.namebadge')->nameBadge(['size'=>45,'middlename'=>false,'uppercase'=>true,'border'=>['width'=>1]]);
+			$this->app->avatar_loaded = true;
+		}
 
 	}
 }
