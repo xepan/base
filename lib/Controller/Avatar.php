cfe1eea1556c9a8c5b23cb6e47c8c05874776dd2
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-04 21:49:40 +0530

    trim names to avoid space in icon

--- lib/Controller/Avatar.php
+++ lib/Controller/Avatar.php
@@ -26,7 +26,7 @@ class Controller_Avatar extends \AbstractController{
 
 		$grid->addHook('formatRow',function($g){			
 			if(!$g->model['image']) 
-				$g->current_row_html['avatar']= "<div class='namebadge' style='position:relative; max-width:50px;float:left'>".$g->model[$this->name_field]."</div>";
+				$g->current_row_html['avatar']= "<div class='namebadge' style='position:relative; max-width:50px;float:left'>".trim($g->model[$this->name_field])."</div>";
 		});
 
 		$this->app->js(true)->_load('jquery.nameBadges')->_selector('.namebadge')->nameBadge(['size'=>45,'middlename'=>false,'uppercase'=>true,'border'=>['width'=>1]]);
