363156e3d1876fbeb4b6eb342a04d02b70b8f23c
sinharakesh <rksinha.btech@gmail.com>
2016-03-10 19:18:16 +0530

    richtext field created

--- lib/Form/Field/RichText.php
+++ lib/Form/Field/RichText.php
@@ -4,12 +4,20 @@
 namespace xepan\base;
 
 class Form_Field_RichText extends \Form_Field_Text{
-	
+	public $options=array();
+
+	function init(){
+		parent::init();
+		$this->addClass('tinymce');
+	}
+
 	function render(){
-		$this->js(true)->
-			_load('bootstrap-wysiwyg')->
-			_load('xepan-richtext')->
-			univ()->richtext();
+
+		$this->js(true)
+				->_load('tinymce/tinymce.min')
+				->_load('tinymce/jquery.tinymce.min')
+				->_load('xepan-richtext');
+		$this->js(true)->univ()->richtext($this,$this->options);
 		parent::render();
 	}
 }
