1c1b7b34c3fd3174447a3a12f9289729724b00b3
vijay mali <guddavijay@gmail.com>
2016-03-10 19:40:58 +0530

    Model Communication Emails Add CallBack Fields & Create View & layout & set to Email Page Spot Block

--- page/generalsetting.php
+++ page/generalsetting.php
@@ -8,7 +8,7 @@ class page_generalsetting extends \Page{
 		$setiingview=$this->add('xepan\hr\CRUD',['action_page'=>'xepan_base_general_email'],'general_setting',['view/setting/email-setting-grid']);
 		$setiingview->setModel('xepan\base\Epan_EmailSetting');
 
-		$this->add('xepan\base\View_Emails',null,'email');
+		// $this->add('xepan\base\View_Emails',null,'email');
 	}
 	function defaultTemplate(){
 		return ['page/general-setting'];
