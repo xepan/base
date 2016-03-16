9dd1a769c60da687b6597402926848eff083c2f7
vijay mali <guddavijay@gmail.com>
2016-03-16 18:08:37 +0530

    Modified forgot password page & jade form layout

--- page/forgotpassword.php
+++ page/forgotpassword.php
@@ -1,31 +1,30 @@
 <?php
 namespace xepan\base;
 class page_forgotpassword extends \Page{
-	public $title="Reset Password";
+	public $title="Forgot Password";
 	function init(){
 		parent::init();
-		$employee=$this->add('xepan\hr\Model_Employee');
-		$employee->load($this->app->employee->id);
-
-		$this->add('H1')->set($employee['name']);
-		$email=$employee->ref('Emails')->tryLoadAny();
 		
 		$form=$this->add('Form');
 		$form->setLayout('layout/xepanforgotpassword');
-		$form->addField('line','email')->set($email['value'])/*->validateNotNull()->validateField('filter_var($this->get(), FILTER_VALIDATE_EMAIL)')*/;
+		$form->addField('line','email');/*->validateNotNull()->validateField('filter_var($this->get(), FILTER_VALIDATE_EMAIL)');*/
 
-		$form->onSubmit(function($f){
-			$email_settings = $this->add('xepan\base\Model_Epan_EmailSetting')->tryLoadAny();
-			$reset_pass=$this->add('xepan\base\Model_Mail_ResetPassword');
-			$reset_pass->tryLoadAny();
-			$mail = $this->add('xepan\communication\Model_Communication_Email');
-			$mail->setfrom($email_settings['from_email'],$email_settings['from_name']);
-			$mail->addTo($f['email']);
-			$mail->setSubject($reset_pass['subject']);
-			$mail->setBody($reset_pass['body']);
-			$mail->send($email_settings);
-			return $f->js()->univ()->successMessage('OK');
-		});
+		if($form->isSubmitted()){
+			$user=$this->add('xepan\base\Model_User');
+			$user->addCondition('username',$form['email']);
+			$user->tryLoadAny();
+			if(!$user->loaded()){
+				$form->displayError('email','Email Id Not Register');
+			}else{
+				$user['hash']=rand(9999,100000);
+				$user->update();
+				
+				$reset_pass=$this->add('xepan\base\Model_Mail_ResetPassword');
+				$reset_pass->sendResetPasswordMail($form['email']);
+
+				return $form->js(null,$form->js()->univ()->successMessage(' E-Mail SuccessFully Send'))->reload->execute();
+			}
+		}
 
 	}
 }
