f473898a0ef961b5adc88048d0096fa00e3ee9da
lenovo <dkanoji93@gmail.com>
2016-04-05 14:56:25 +0530

    Namespace Bug Solved

--- lib/Menu/SideBar.php
+++ lib/Menu/SideBar.php
@@ -35,7 +35,7 @@ class Menu_SideBar extends \Menu_Advanced{
         }
 
         if ($action) {
-            if (is_string($action) || is_array($action) || $action instanceof URL) {
+            if (is_string($action) || is_array($action) || $action instanceof \URL) {
                 $i->template->set('url',$url = $this->app->url($action));
                 if($url->isCurrent()){
                     $i->addClass('active');
