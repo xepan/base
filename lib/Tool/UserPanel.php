2e69c94fd12e291d4a2f17bf196630118178a35b
vijay mali <guddavijay@gmail.com>
2016-04-13 16:52:41 +0530

    form spot jade add form error class

--- lib/Tool/UserPanel.php
+++ lib/Tool/UserPanel.php
@@ -13,41 +13,45 @@ class Tool_UserPanel extends \xepan\cms\View_Tool{
 			$this->options['layout']=$layout;
 		}
 
-		$view_url = $this->api->url(null,['cut_object'=>$this->name]);
+		if(!$this->app->auth->isLoggedIn()){
+			$view_url = $this->api->url(null,['cut_object'=>$this->name]);
 
-		switch ($this->options['layout']) {
-			case 'login_view':
-				$user_login=$this->add('xepan\base\View_User_LoginPanel',array('options'=>$this->options));
-				
-				$user_login->on('click','a.forgotpassword',function($js,$data)use($view_url){
-					return $this->js()->reload(['layout'=>'forget_password'],null,$view_url);
-				});
+			switch ($this->options['layout']) {
+				case 'login_view':
+					$user_login=$this->add('xepan\base\View_User_LoginPanel',array('options'=>$this->options));
+					
+					$user_login->on('click','a.forgotpassword',function($js,$data)use($view_url){
+						return $this->js()->reload(['layout'=>'forget_password'],null,$view_url);
+					});
 
-				$user_login->on('click','a.reg_view',function($js,$data)use($view_url){
-					return $this->js()->reload(['layout'=>'new_registration'],null,$view_url)->execute();
-				});
-				$this->app->stickyForget('layout');
-			break;
-			case 'forget_password':
-				$f_view=$this->add('xepan\base\View_User_ForgotPassword',array('options'=>$this->options));
-				
-				$f_view->on('click','a.back-login',function($js,$data)use($view_url){
-					return $this->js()->reload(['layout'=>'login_view'],null,$view_url)->execute();
-				});
+					$user_login->on('click','a.reg_view',function($js,$data)use($view_url){
+						return $this->js()->reload(['layout'=>'new_registration'],null,$view_url)->execute();
+					});
+					$this->app->stickyForget('layout');
+				break;
+				case 'forget_password':
+					$f_view=$this->add('xepan\base\View_User_ForgotPassword',array('options'=>$this->options));
+					
+					$f_view->on('click','a.back-login',function($js,$data)use($view_url){
+						return $this->js()->reload(['layout'=>'login_view'],null,$view_url)->execute();
+					});
 
-				$this->app->stickyForget('options');
+					$this->app->stickyForget('options');
 
-			break;
-			case 'new_registration':
-				$r_view=$this->add('xepan\base\View_User_Registration',array('options'=>$this->options));
-				$r_view->on('click','a.back-login',function($js,$data)use($view_url){
-					return $this->js()->reload(['layout'=>'login_view'],null,$view_url)->execute();
-				});
+				break;
+				case 'new_registration':
+					$r_view=$this->add('xepan\base\View_User_Registration',array('options'=>$this->options));
+					$r_view->on('click','a.back-login',function($js,$data)use($view_url){
+						return $this->js()->reload(['layout'=>'login_view'],null,$view_url)->execute();
+					});
 
-				$this->app->stickyForget('options');
-			break;
+					$this->app->stickyForget('options');
+				break;
+			}
+			
+		}else{
+			$this->add('View')->setHTML('Allready Logged In');
 		}
-		
 	}
 
 }
