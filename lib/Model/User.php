67a62389f749d4290c684bf2b1a8499d666e6353
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-18 16:41:17 +0530

    User doesn't knoe HR/employee application here

--- lib/Model/User.php
+++ lib/Model/User.php
@@ -27,7 +27,7 @@ class Model_User extends \xepan\base\Model_Table{
 		parent::init();
 		
 		$this->hasOne('xepan\base\Epan');
-		$this->hasOne('xepan\hr\Employee','created_by_id');
+		$this->hasOne('xepan\base\Contact','created_by_id');
 
 		$this->addField('username');
 		$this->addField('password')->type('password');
