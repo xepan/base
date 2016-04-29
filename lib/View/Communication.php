b5b2cf31a8058ea46acffb444f4b15e44f1387a8
vaibhav <vaibhavsharma38@gmail.com>
2016-04-29 11:51:32 +0530

    email communication with customer done, informing left

--- lib/View/Communication.php
+++ lib/View/Communication.php
@@ -12,19 +12,28 @@ class View_Communication extends \CompleteLister{
 		$to_lister = $this->app->add('CompleteLister',null,null,['view/communication1','to_lister']);
 		$to_lister->setSource($to_mail);
 			
-		$cc_raw = json_decode($this->model['cc'],true);
+		$cc_raw = json_decode($this->model['cc_raw'],true);
 		$cc_lister = $this->app->add('CompleteLister',null,null,['view/communication1','cc_lister']);
 		$cc_lister->setSource($cc_raw);
 
-		// echo"<pre>";
-		// print_r($to_mail);
-		// exit;
-		// throw new \Exception($this->model['attachment_count']);
+		$from_mail = json_decode($this->model['from_raw'],true);
+		$from_lister = $this->app->add('CompleteLister',null,null,['view/communication1','from_lister']);
+		$from_lister->setSource($from_mail);
+
+		$attach=$this->app->add('CompleteLister',null,null,['view/communication1','Attachments']);
+		$attach->setModel('xepan\communication\Communication_Attachment')->addCondition('communication_email_id',$this->model->id);
 		
-		$this->current_row_html['description']=$this->current_row['description'];
-		$this->current_row_html['attachment'] = $this->model['attachment_count']?'<span><i style="color:green" class="fa fa-paperclip"></i></span>':'';
+		$this->current_row_html['description'] = $this->current_row['description'];
+		
+		if($this->model['attachment_count'])
+			$this->current_row_html['attachment'] = '<span><i style="color:green" class="fa fa-paperclip"></i></span>';
+		else
+			$this->current_row_html['attachment']='';
+
 		$this->current_row_html['to_lister'] = $to_lister->getHtml();
-		// $this->current_row_html['cc_lister'] = $cc_lister->getHtml();
+		$this->current_row_html['cc_lister'] = $cc_lister->getHtml();
+		$this->current_row_html['from_lister'] = $from_lister->getHtml();
+		$this->current_row_html['Attachments'] = $attach->getHtml();
 		return parent::formatRow();
 	}
 
