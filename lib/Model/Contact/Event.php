6ef1c8a1c06a806408a675e8b9b673218b3a5ec2
vaibhav <vaibhavsharma38@gmail.com>
2016-05-11 17:52:50 +0530

    bla removed

--- lib/Model/Contact/Event.php
+++ lib/Model/Contact/Event.php
@@ -16,7 +16,7 @@ class Model_Contact_Event extends Model_Contact_Info{
 	function init(){
 		parent::init();
 			
-		$this->getElement('head')->enum(['DOB','Anniversary','Bla']);
+		$this->getElement('head')->enum(['DOB','Anniversary']);
 		$this->addCondition('type','Event');
 	}
 }
