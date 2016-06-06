d63b15f6b1876cfda619d80eec8e4d95354834c6
sinharakesh <rksinha.btech@gmail.com>
2016-06-06 15:22:04 +0530

    jquery ui icon and test page update with deisgn url

--- page/test.php
+++ page/test.php
@@ -17,15 +17,32 @@ class page_test extends \Page {
 	function init(){
 		parent::init();
 
-		$g=$this->add('Grid');
-		$g->setModel('xepan\base\Epan');
-		$g->addQuickSearch(['name']);
-
-		$f= $this->add('Form');
-		$f->addField('line','q');
-		if($f->isSubmitted()){
-			throw new \Exception($f['q'], 1);
+		$items = $this->add("xepan\commerce\Model_Item");
+		foreach ($items as $item) {
+			if(!$item['designs'])
+				continue;
 			
+			$replace = str_replace("\/upload", "websites\/www\/upload", $item['designs']);
+			$item['designs'] = $replace;
+			$item->save();			
 		}
+		// $design_array = json_decode($item['designs'],true);
+		// $design = $design_array['design']; 
+		// echo $item['designs'];
+		// echo "<pre>";
+		// print_r($design);
+		// foreach ($design as $page) {
+		// 	$design = json_encode($page);
+		// }
+		// $g=$this->add('Grid');
+		// $g->setModel('xepan\base\Epan');
+		// $g->addQuickSearch(['name']);
+
+		// $f= $this->add('Form');
+		// $f->addField('line','q');
+		// if($f->isSubmitted()){
+		// 	throw new \Exception($f['q'], 1);
+			
+		// }
 	}
 }
