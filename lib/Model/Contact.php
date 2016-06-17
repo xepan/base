1300eba521df4c6e516870ca7e3f633301cca87c
dkanojia <dkanojia93@gmail.com>
2016-06-17 12:19:30 +0530

    required condition checked

--- lib/Model/Contact.php
+++ lib/Model/Contact.php
@@ -89,9 +89,7 @@ class Model_Contact extends \xepan\base\Model_Table{
 				'first_name|to_trim|to_upper_words|required',
 				'last_name|to_trim|to_upper_words',
 				'user_id|unique_in_epan',
-				'type|to_trim|required',
-				'country_id|required',
-				'state_id|required'
+				'type|to_trim|required'
 			]);
 	}
 
