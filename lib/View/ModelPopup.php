aff3406ba04ad9746cebdc9090e2a875b135b33a
sinharakesh <rksinha.btech@gmail.com>
2016-06-18 13:34:34 +0530

    modelpopup view added

---
+++ lib/View/ModelPopup.php
@@ -0,0 +1,64 @@
+<?php
+namespace xepan\base;
+
+class View_ModelPopup extends \View{
+	public $options = [
+					'addCloseButton'=>true,
+					'close_button_label' => "Close",
+					'addSaveButton'=>true,
+					'save_button_label' => "Save Changes"
+				];
+
+	function init(){
+		parent::init();
+	}
+
+
+	function setModelSize(){
+
+	}
+
+	function addCloseButton(){
+		$this->add('Button',null,'close_button')
+				->set($this->options['close_button_label'])
+				->setAttr(array("data-dismiss"=>'model',"type"=>"button"))
+				->addClass('btn btn-default');
+	}
+
+	function addSaveButton(){
+		$save_btn = $this->add('Button',null,'save_button');
+		$save_btn->set($this->options['save_button_label'])
+			->setAttr(array("type"=>"button"))
+			->addClass('btn btn-primary');
+
+		$save_btn->js('click',$this->js()->find('.atk-form')->atk4_form("submitForm"));
+
+	}
+
+	function setTitle($title){
+		$this->template->trySet('title',$title);
+	}
+
+	function setSaveButtonLabel($label){
+		$this->template->trySet('save_button_label',$label);
+	}
+
+	function setCloseButtonLabel($label){
+		$this->template->trySet('close_button_label',$label);
+	}
+
+	function defaultTemplate(){
+		return ['view/modelpopup'];
+	}
+	
+	function recursiveRender(){
+		if($this->options['addCloseButton'])
+			$this->addCloseButton();
+
+		if($this->options['addSaveButton'])
+			$this->addSaveButton();
+
+		parent::recursiveRender();
+	}
+
+}
