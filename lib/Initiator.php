1612e3a9f179f662abd5091f0c09f4ab87129804
vijay mali <guddavijay@gmail.com>
2016-06-21 20:13:28 +0530

    bootstrap js include

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -132,7 +132,9 @@ class Initiator extends \Controller_Addon {
         $this->app->jui->addStaticStyleSheet('pnotify.custom.min');
         $this->app->jui->addStaticStyleSheet('animate');
         $this->app->jui->addStaticInclude('xepan_jui');
+        $this->app->jui->addStaticInclude('xepan_jui');
         $this->app->jui->addStylesheet('bootstrap/bootstrap.min');
+        $this->app->jui->addStaticInclude('bootstrap.min');
 
         $auth = $this->app->add('BasicAuth',['login_layout_class'=>'xepan\base\Layout_Login']);
         $auth->usePasswordEncryption('md5');
