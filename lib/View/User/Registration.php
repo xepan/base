03a680233ea26cd79c40acf49b59c1fb90794e05
vaibhav <vaibhavsharma38@gmail.com>
2016-06-21 15:32:45 +0530

    registration view (frontend) changed

--- lib/View/User/Registration.php
+++ lib/View/User/Registration.php
@@ -45,6 +45,10 @@ class View_User_Registration extends \View{
 					$email_settings = $this->add('xepan\communication\Model_Communication_EmailSetting')->tryLoadAny();
 					$mail = $this->add('xepan\communication\Model_Communication_Email');
 
+					$merge_model_array=[];
+					$merge_model_array = array_merge($merge_model_array,$user->get());
+					$merge_model_array = array_merge($merge_model_array,$contact->get());
+
 					$reg_model=$this->app->epan->config;
 					$email_subject=$reg_model->getConfig('REGISTRATION_SUBJECT');
 					$email_body=$reg_model->getConfig('REGISTRATION_BODY');
@@ -59,18 +63,23 @@ class View_User_Registration extends \View{
 												]
 										)->useAbsoluteURL();
 
-					$tag_url="<a href=\"".$url."\">Click Here to Activate </a>"	;
-							
-					$temp->trySetHTML('name',$contact['name']);
-					
-					$temp->trySetHTML('otp',$user['hash']);
-					$temp->trySetHTML('password',$user['password']);
-					$temp->trySetHTML('email_id',$user['username']);
-					$temp->trySetHTML('click_here_to_activate',$tag_url);
+					$tag_url="<a href=\"".$url."\">Click Here </a>"	;
+
+					$subject_temp=$this->add('GiTemplate');
+					$subject_temp->loadTemplateFromString($email_subject);
+					$subject_v=$this->add('View',null,null,$subject_temp);
+					$subject_v->template->trySet($merge_model_array);
+
+					$temp=$this->add('GiTemplate');
+					$temp->loadTemplateFromString($email_body);
+					$body_v=$this->add('View',null,null,$temp);
+					$body_v->template->trySet($merge_model_array);					
+					$body_v->template->trySetHTML('click_here',$tag_url);		
+										
 					$mail->setfrom($email_settings['from_email'],$email_settings['from_name']);
 					$mail->addTo($f['email_id']);
-					$mail->setSubject($email_subject);
-					$mail->setBody($temp->render());
+					$mail->setSubject($subject_v->getHtml());
+					$mail->setBody($body_v->getHtml());
 					$mail->send($email_settings);						
 				}
 				
