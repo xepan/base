f5b786a5a10062abb1803a97050fdb0e0469fb3c
vijay mali <guddavijay@gmail.com>
2016-06-29 14:58:33 +0530

    schema microdata view & jade created

---
+++ lib/View/SchemaMicroData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace xepan\base;
+
+/**
+* 
+*/
+class View_SchemaMicroData extends \View{
+	function init(){
+		parent::init();	
+	}
+
+	function defaultTemplate(){
+		return ['view/schema-micro-data'];
+	}
+}
