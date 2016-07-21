5c6d9cfec694b39a4d3cf1d34fb0fc5c9b3508d7
dkanojia <dkanojia93@gmail.com>
2016-07-21 20:24:03 +0530

    activity grid finalized

--- lib/View/Activity.php
+++ lib/View/Activity.php
@@ -6,22 +6,31 @@ class View_Activity extends \View{
 	function init(){
 	parent::init();
 
-	$contact_array = [
-						"Lead" => "xepan_marketing_leaddetails",
-						"Customer" => "xepan_commerce_customerdetail",
-						"Supplier" => "xepan_commerce_supplierdetail",
-						"Employee" => "xepan_hr_employeedetail"
-					];
-
-
 	$activity_model=$this->add('xepan\base\Model_Activity');
 	$activity_model->addExpression('contact_type',$activity_model->refSQL('related_contact_id')->fieldQuery('type'));
 	
 	$grid = $this->add('xepan\base\Grid',null,null,['view/activity/activities']);
 	$grid->setModel($activity_model);
 
-	$grid->js('click')->_selector('.do-view-person-frame')->univ()->frameURL('Customer Details',[$this->api->url('xepan_commerce_customerdetail'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-contact-id]')->data('contact-id')]);
-
+	$grid->addHook('formatRow',function($g){
+		switch($g->model['contact_type']){
+			case 'Lead':
+				$contact_url='xepan_marketing_leaddetails'.'&contact_id='.$g->model['related_contact_id'];
+				break;
+			case 'Customer':
+				$contact_url='xepan_commerce_customerdetail'.'&contact_id='.$g->model['related_contact_id'];
+				break;
+			case 'Supplier':
+				$contact_url='xepan_commerce_supplierdetail'.'&contact_id='.$g->model['related_contact_id'];
+				break;
+			case 'Employee':
+				$contact_url='xepan_hr_employeedetail'.'&contact_id='.$g->model['related_contact_id'];
+				break;
+			default:
+				$contact_url='xepan_base_contactdetail'.'&contact_id='.$g->model['related_contact_id'];
+		}
+		$g->current_row['contact_url']= $contact_url;
+	});
 
 	}
 }
