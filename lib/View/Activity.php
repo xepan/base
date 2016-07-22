5d6e425a3316be80d67aeeb59544981e49a203e2
dkanojia <dkanojia93@gmail.com>
2016-07-22 11:00:31 +0530

    format row condtion added

--- lib/View/Activity.php
+++ lib/View/Activity.php
@@ -33,8 +33,13 @@ class View_Activity extends \View{
 	});
 
 	$grid->addHook('formatRow',function($g){
-			if(!$g->model['related_document_id']) $g->current_row_html['related_document_id']= "sNot Available";
+			if(!$g->model['related_document_id']) $g->current_row_html['related_document_id']= "Not Available";
 	});
+
+	$grid->addHook('formatRow',function($g){
+			if(!$g->model['related_contact_id']) $g->current_row_html['related_contact']= "Not Available";
+	});
+
 	$grid->addPaginator(10);
 
 	}
