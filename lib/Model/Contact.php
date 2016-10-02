300cfd0c0a99ff015d3b932ec30031fb7ff2ecb0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-10-02 16:42:48 +0530

    Proper dereference fields for same class (Contact)

--- lib/Model/Contact.php
+++ lib/Model/Contact.php
@@ -24,8 +24,8 @@ class Model_Contact extends \xepan\base\Model_Table{
 		parent::init();
 
 		$this->hasOne('xepan\base\Epan');
-		$this->hasOne('xepan\base\Contact','created_by_id');
-		$this->hasOne('xepan\base\Contact','assign_to_id');
+		$this->hasOne('xepan\base\ContactCreatedBy','created_by_id');
+		$this->hasOne('xepan\base\ContactAssignedTo','assign_to_id');
 		$this->hasOne('xepan\base\User',null,'username');
 		$this->hasOne('xepan\base\Country','country_id')->display(array('form' => 'xepan\commerce\DropDown'));
 		$this->hasOne('xepan\base\State','state_id')->display(array('form' => 'xepan\commerce\DropDown'));
---
+++ lib/Model/ContactCreatedBY.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace xepan\base;
+
+class Model_ContactCreatedBY extends Model_Contact {
+	public $table_alias='created_by_alias';
+} 
