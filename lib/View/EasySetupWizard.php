92c4f6cfe9747d0a93f275bcce71c945814ab399
dkanojia <dkanojia93@gmail.com>
2016-10-07 19:48:04 +0530

    Easy Setup Wizard

--- lib/View/EasySetupWizard.php
+++ lib/View/EasySetupWizard.php
@@ -25,7 +25,40 @@ class View_EasySetupWizard extends \View{
 
 		$country_view->setAddOn('Application - Base')
 			->setTitle('Country & State database not populated yet')
-			->setMessage('Populate country and states. Need help ! click on the help icon')
+			->setMessage('Populate country and states.')
+			->setHelpMessage('Need help ! click on the help icon')
+			->setHelpURL('#')
+			->setAction('Click Here',$action,$isDone);
+
+		if($_GET[$this->name.'_time_zone']){
+			
+			$this->js(true)->univ()->frameURL("Time Zone",$this->app->url('xepan_communication_generalsetting'));
+		}
+
+		$isDone = false;
+		
+		$action = $this->js()->reload([$this->name.'_time_zone'=>1]);
+		$misc_m = $this->add('xepan\base\Model_ConfigJsonModel',
+		[
+			'fields'=>[
+						'time_zone'=>'DropDown'
+						],
+				'config_key'=>'Miscellaneous_Technical_Settings',
+				'application'=>'base'
+		]);
+		$misc_m->tryLoadAny();	
+
+		if($misc_m['time_zone']){
+			$isDone = true;
+			$action = $this->js()->univ()->dialogOK("Already have Data",' You already have updated the time zone, visit page ? <a href="'. $this->app->url('xepan_communication_generalsetting')->getURL().'"> click here to go </a>');
+		}	
+
+		$time_zone_view = $this->add('xepan\base\View_Wizard_Step');
+
+		$time_zone_view->setAddOn('Application - Base')
+			->setTitle('Update Your Time Zone')
+			->setMessage('Update your time zone accoding your country.')
+			->setHelpMessage('Need help ! click on the help icon')
 			->setHelpURL('#')
 			->setAction('Click Here',$action,$isDone);
 	}
