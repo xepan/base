f507c54b5475ff48cd811e3cfcca06b65ec1c4f7
vaibhav <vaibhavsharma38@gmail.com>
2016-11-15 10:47:42 +0530

    order field added

--- lib/Model/GraphicalReport/Widget.php
+++ lib/Model/GraphicalReport/Widget.php
@@ -12,6 +12,7 @@ class Model_GraphicalReport_Widget extends \xepan\base\Model_Table{
 		$this->hasOne('xepan\base\GraphicalReport','graphical_report_id');
 		$this->addField('name');
 		$this->addField('col_width')->enum([1,2,3,4,5,6,7,8,9,10,11,12]);
+		// $this->addField('order');
 		// allowed to which post :: hasmany or json {"12","23"} like id search  
 		$this->addField('class_path');
 	}
