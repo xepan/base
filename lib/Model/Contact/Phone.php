9def05e0800baf54e7e677e7b3067620f054eb0a
dkanojia <dkanojia93@gmail.com>
2016-12-08 20:14:24 +0530

    model config updated

--- lib/Model/Contact/Phone.php
+++ lib/Model/Contact/Phone.php
@@ -18,5 +18,41 @@ class Model_Contact_Phone extends Model_Contact_Info{
 			
 		$this->getElement('head')->enum(['Official','Personal','Mobile']);
 		$this->addCondition('type','Phone');
+
+		$this->addHook('beforeSave',[$this,'checkPhoneNo']);
 	}
+
+	function checkPhoneNo(){
+        $contact = $this->add('xepan\base\Model_Contact');
+        
+        if($this['contact_id'])
+	        $contact->load($this['contact_id']);
+
+		$contactconfig_m = $this->add('xepan\base\Model_ConfigJsonModel',
+			[
+				'fields'=>[
+							'contact_no_duplcation_allowed'=>'DropDown'
+							],
+					'config_key'=>'Contact No Duplication Allowed Settings',
+					'application'=>'base'
+			]);
+		$contactconfig_m->tryLoadAny();	
+
+		if($contactconfig_m['contact_no_duplcation_allowed'] != 'Duplication Allowed'){
+	        $contactphone_m = $this->add('xepan\base\Model_Contact_Phone');
+	        $contactphone_m->addCondition('id','<>',$this->id);
+	        $contactphone_m->addCondition('value',$this['value']);
+			
+			if($contactconfig_m['contact_no_duplcation_allowed'] == 'No Duplication Allowed'){
+				$contactphone_m->addCondition('contact_type',$this['contact_type']);
+		        $contactphone_m->tryLoadAny();
+		 	}
+
+	        $contactphone_m->tryLoadAny();
+	        
+	        if($contactphone_m->loaded())
+	            throw $this->exception('This Contact No. Already Used','ValidityCheck')->setField('value');
+		}	
+    }
+
 }
