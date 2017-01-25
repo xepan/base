b8503c309b7cdb136c24248d5fbb9cf1f8be4a37
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-01-25 17:13:45 +0530

    base code editor field updated

--- lib/Form/Field/CodeEditor.php
+++ lib/Form/Field/CodeEditor.php
@@ -7,9 +7,11 @@ class Form_Field_CodeEditor extends \Form_Field_Text{
 	public $options=array();
 	public $lang='html';
 	public $theme='tomorrow';
+	public $worker='html';
 
 	function init(){
 		parent::init();
+		$this->setRows(10);
 	}
 
 	function render(){
@@ -17,9 +19,10 @@ class Form_Field_CodeEditor extends \Form_Field_Text{
 				->_load('ace/ace/ace')
 				->_load('ace/ace/mode-'.$this->lang)
 				->_load('ace/ace/theme-'.$this->theme)
+				->_load('ace/ace/worker-'.$this->worker)
 				->_load('ace/jquery-ace.min');
 
-		$this->js(true)->ace(['theme' =>$this->theme, 'lang'=>$this->lang]);
+		$this->js(true)->ace(['theme' =>$this->theme, 'lang'=>$this->lang, 'width'=>'100%']);
 		parent::render();
 	}
 }
