21051ea4d7ac1c2941594d3e4bfdd7aea67c6c76
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-01-26 11:28:20 +0530

    Allow superUser to multidelete

--- lib/Controller/MultiDelete.php
+++ lib/Controller/MultiDelete.php
@@ -11,7 +11,8 @@ class Controller_MultiDelete extends \AbstractController {
 
 	function init(){
         parent::init();
-        return;
+        
+        if(!$this->app->auth->model->isSuperUser()) return;
 
         if($this->owner instanceof \CRUD ){
         	if($this->owner->isEditing())
