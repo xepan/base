c4edd1c4145769a9d59528b474f1b04e533a341e
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-02-10 11:45:43 +0530

    changes made in widgets

--- lib/Widget/RecentContacts.php
+++ lib/Widget/RecentContacts.php
@@ -11,6 +11,7 @@ class Widget_RecentContacts extends \xepan\base\Widget{
 
 	function recursiveRender(){
 		$contact = $this->add('xepan\base\Model_Contact');
+		$contact->addCondition('status','Active');
 		$contact->setOrder('created_at','desc');
 
 		$this->grid->setModel($contact,['name','created_at','created_by','type']);
