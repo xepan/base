a9b180778dd91e1435d8301e93fe497f70068bfe
vijay mali <guddavijay@gmail.com>
2017-02-15 16:11:15 +0530

    called to added

--- lib/Model/Contact/CommunicationReadEmail.php
+++ lib/Model/Contact/CommunicationReadEmail.php
@@ -13,6 +13,7 @@ class Model_Contact_CommunicationReadEmail extends \xepan\base\Model_Table{
 		$this->hasOne('xepan\hr\Employee','contact_id');
 		$this->hasOne('xepan\communication\Communication','communication_id');
 		$this->addField('is_read')->type('boolean')->defaultValue(false);
-		$this->addField('type');
+		$this->addField('type');/*FROM TO CC BCC*/
+		$this->addField('row');
 	}
 }
