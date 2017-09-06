0baed6979d9f59511f4bdd00a605aaea63cb2059
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-09-06 19:12:06 +0530

    logout will not do anything in employee in base it just throws a hook

--- page/logout.php
+++ page/logout.php
@@ -8,47 +8,10 @@ class page_logout extends \xepan\base\Page{
 		parent::init();
 
 		$this->app->hook('logout_page',[$this]);
-		$movement = $this->add('xepan\hr\Model_Employee_Movement');
-		
-		// No Form ... just logout
-		// $form = $this->add('Form');
-		// $form->setModel($movement,['reason','narration']);
-		// $form->addSubmit('Logout')->addClass('btn btn-primary');
-
-
-		// if($form->isSubmitted()){
-		// 	if(!$form['reason'])
-		// 		$form->displayError('reason','Reason is mandatory');
 						
-			$movement->addCondition('employee_id',$this->app->employee->id);
-			$movement->addCondition('movement_at',$this->app->now);
-			$movement->addCondition('direction','Out');
-			// $movement->addCondition('reason',$form['reason']);
-			// $movement->addCondition('narration',$form['narration']);
-			$movement->save();
-
-			$attan_m = $this->add("xepan\hr\Model_Employee_Attandance");
-			$attan_m->addCondition('employee_id',$this->app->employee->id);
-			$attan_m->addCondition('fdate',$this->app->today);
-			$attan_m->setOrder('id','desc');
-			$attan_m->tryLoadAny();
-			if($attan_m->loaded()){
-				
-
-			// initially it was considered that official outing is not actually outing, you are working
-			// for office but just out of premises... then changed.. if it is so .. just don't log out
-			// and make task what you are doing where ??
-
-			// if($movement['reason'] != 'Official Outing'){
-				$attan_m['to_date'] = $this->app->now;
-				$attan_m['total_movement_out'] = $attan_m['total_movement_out'] + 1;
-				$attan_m->save();
-			// }
-			}
-									
-			$this->app->hook('user_loggedout',[$this->app->auth->model,$this->app->employee]);
-			$this->app->auth->logout();
-			$this->app->redirect('/');
-		// }
+		$this->app->hook('user_loggedout',[$this->app->auth->model]);
+		$this->app->auth->logout();
+		$this->app->redirect('/');
+		
 	}
 }
