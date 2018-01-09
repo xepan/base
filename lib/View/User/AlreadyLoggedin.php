10f7e47527eb8483483d3501497cc49bd2cc76ad
sinharakesh <rksinha.btech@gmail.com>
2018-01-09 19:22:53 +0530

    userpanel if logged in then redirect to after login page

--- lib/View/User/AlreadyLoggedin.php
+++ lib/View/User/AlreadyLoggedin.php
@@ -10,6 +10,9 @@ class View_User_AlreadyLoggedin extends \View{
 		$auth = $this->app->auth;
 		$auth->login($this->app->auth->model['username']);
 		$this->app->hook('login_panel_user_loggedin',[$auth->model]);
+
+		$this->app->redirect($this->app->url($this->options['login_success_url']));
+		
 	}
 	
 	function defaultTemplate(){
