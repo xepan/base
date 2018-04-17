927d11286b814edce47b94cf372147f2415c0233
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-04-17 19:25:25 +0530

    implmeneted AuditLog ... add in model

---
+++ lib/Controller/AuditLog.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace xepan\base;
+
+class Controller_AuditLog extends \AbstractController{
+	function init(){
+		parent::init();
+
+		if(!$this->owner instanceof \xepan\base\Model_Table){
+			throw $this->exception('Owner of Controller_AuditLog must be a sql_model');
+		}
+
+		$this->owner->addHook('beforeSave',function($model){
+			if(@$model->app->skip_audit_log) return;
+			
+			if($model->loaded()){
+				$old_m = $model->newInstance()->load($model->id);
+				$changes=array();
+				foreach ($model->dirty as $dirty_field=>$changed) {
+					if($old_m[$dirty_field] != $model[$dirty_field])
+						$changes[$dirty_field]=array('from'=>$old_m[$dirty_field],'to'=>$model[$dirty_field]);
+				}
+
+				if(!count($changes)) return;
+
+				$log = $model->add('xepan\base\Model_AuditLog');
+				$log['model_class'] = get_class($model);
+				$log['pk_id'] = $model->id;
+				$log['name'] = json_encode($changes);
+				$log['type'] = "Edit";
+				$log['contact_id'] = @$this->app->employee->id;
+				$log->save();
+			}
+		});
+
+		$this->owner->addHook('beforeDelete',function($model){
+
+				if(@$model->app->skip_audit_log) return;
+				$log = $model->add('xepan\base\Model_AuditLog');
+				$log['model_class'] = get_class($model);
+				$log['pk_id'] = $model->id;
+				$log['name'] = json_encode($model->data);
+				$log['type'] = "Delete";
+				$log['contact_id'] = @$this->app->employee->id;
+				$log->save();
+		});
+	}
+}
