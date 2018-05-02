984a1c23b79439fe52a08ddf07d27f396a7c692e
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-05-02 14:05:37 +0530

    set all width height in carousel to varchar not just int

---
+++ dbversion/00000227.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `carousellayer` MODIFY COLUMN `width`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+ALTER TABLE `carousellayer` MODIFY COLUMN `height`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+ALTER TABLE `carouselcategory` MODIFY COLUMN `height`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
