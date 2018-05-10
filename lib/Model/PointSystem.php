ae2cb4e031abd9090262fa9a208181d6dc976c87
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-05-10 10:03:53 +0530

    working on point system

--- lib/Model/PointSystem.php
+++ lib/Model/PointSystem.php
@@ -25,8 +25,33 @@ class Model_PointSystem extends \xepan\base\Model_Table
 		$this->addField('landing_content_id')->defaultValue(0);
 		$this->addField('created_at')->type('datetime')->defaultValue(@$this->app->now);
 
+		$this->addExpression('created_at_date')->set('DATE(created_at)')->type('date');
+
+		$this->addHook('beforeSave',$this);
 		$this->addHook('afterSave',$this);
 		$this->addHook('beforeDelete',$this);
+	}
+
+	function beforeSave(){
+
+		if($this->isDirty('qty') && $this->loaded()){
+			$old_score = $this->newInstance()->load($this->id)->get('score');
+			
+			$contact_m = $this->add('xepan\base\Model_Contact');
+			$contact_m->addCondition('id',$this['contact_id']);
+			$contact_m->tryLoadAny();
+
+			if($contact_m->loaded()){
+				$contact_m['score'] = $contact_m['score'] - $old_score;
+				$contact_m->save();
+			}
+		}
+
+		if($this['rule_option_id'])
+			$this['score']= $this['qty'] * $this->ref('rule_option_id')->get('score_per_qty');
+
+		if($this['rule_option_id'] && !$this['rule_id'])
+			$this['rule_id'] = $this->ref('rule_option_id')->get('rule_id');
 	}
 
 	function afterSave($m){
