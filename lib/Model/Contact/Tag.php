b11255d409759e9b36bd9e572722f39dd7d96c74
sinharakesh <rksinha.btech@gmail.com>
2018-05-30 18:58:32 +0530

    tag config and fields added

---
+++ lib/Model/Contact/Tag.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace xepan\base;
+
+class Model_Contact_Tag extends \xepan\base\Model_ConfigJsonModel{
+	public  $fields	= [
+					'name'=>"Line"
+				];
+
+	public $config_key = 'XEPAN_BASE_CONTACT_TAG';
+	public $application = 'base';
+
+	function init(){
+		parent::init();
+	
+	}
+}
