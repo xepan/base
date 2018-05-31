5c56ca24dce5dcd25c5718b1dc100520e2462401
sinharakesh <rksinha.btech@gmail.com>
2018-05-31 13:26:23 +0530

    sanitize saved with ` sign

--- lib/Model/Contact/Tag.php
+++ lib/Model/Contact/Tag.php
@@ -9,4 +9,14 @@ class Model_Contact_Tag extends \xepan\base\Model_ConfigJsonModel{
 
 	public $config_key = 'XEPAN_BASE_CONTACT_TAG';
 	public $application = 'base';
+
+	function init(){
+		parent::init();
+
+		$this->addHook('beforeSave',[$this,'sanitize'],[],4);
+	}
+
+	function sanitize(){
+		$this['name'] = "`".str_replace('`', "", trim($this['name']))."`";
+	}
 }
