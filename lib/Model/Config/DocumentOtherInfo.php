4ed7c9fe62088055e221f5dc784139d3307101c5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-05 23:08:43 +0530

    basic Document Other Info action runing, TODO Save in Table and Load Saved values

--- lib/Model/Config/DocumentOtherInfo.php
+++ lib/Model/Config/DocumentOtherInfo.php
@@ -17,12 +17,13 @@ class Model_Config_DocumentOtherInfo extends \xepan\base\Model_ConfigJsonModel{
 
 	function init(){
 		parent::init();
-		$this->getElement('type')->enum(['Line','Text','DatePicker']);
+		$this->getElement('type')->enum(['Line','Text','DropDown','DatePicker']);
 		$this->getElement('conditional_binding')->hint("Enter sperated lines for fields \n{'Value A':{'Field B', 'Field C','Field D'},'Value B':{'Field A', 'Field F'}}");
 		$this->getElement('possible_values')->hint('Comma Seperated Values For DropDown type');
 		$this->getElement('for')->hint('Document Type like Quotation/SalesOrde/SalesInvoice/PurhcaseOrder etc');
 
 		$this->addHook('beforeSave',function($m){
+			$this['conditional_binding'] = trim($this['conditional_binding']);
 			if($this['conditional_binding']){
 				json_decode($this['conditional_binding']);
 				if(json_last_error() !== JSON_ERROR_NONE){
