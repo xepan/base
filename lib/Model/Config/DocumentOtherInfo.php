48dbd86ff51f4f0a9c08dee7c8c798c24c0d8f34
sinharakesh <rksinha.btech@gmail.com>
2018-06-06 07:48:54 +0530

    document other info hint corrected and mandatory applied on form

--- lib/Model/Config/DocumentOtherInfo.php
+++ lib/Model/Config/DocumentOtherInfo.php
@@ -18,7 +18,7 @@ class Model_Config_DocumentOtherInfo extends \xepan\base\Model_ConfigJsonModel{
 	function init(){
 		parent::init();
 		$this->getElement('type')->enum(['Line','Text','DropDown','DatePicker']);
-		$this->getElement('conditional_binding')->hint("Enter sperated lines for fields \n{'Value A':{'Field B', 'Field C','Field D'},'Value B':{'Field A', 'Field F'}}");
+		$this->getElement('conditional_binding')->hint('Enter sperated lines for fields \n{"Value A":["Field B", "Field C","Field D"],"Value B":["Field A", "Field F"]}');
 		$this->getElement('possible_values')->hint('Comma Seperated Values For DropDown type');
 		$this->getElement('for')->hint('Document Type like Quotation/SalesOrde/SalesInvoice/PurhcaseOrder etc');
 
